d681ff4119f399686236652d9dfb674304dbc461
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-11-23 19:10:02 +0600

    Add files via upload

---
+++ chapterEleven/src/chapterEleven/Student.java
@@ -0,0 +1,21 @@
+package chapterEleven;
+
+class Student extends Person {
+    public static final String FRESHMAN = "Freshman";
+    public static final String SOPHOMORE = "Sophomore";
+    public static final String JUNIOR = "Junior";
+    public static final String SENIOR = "Senior";
+
+    private String status;
+
+    public Student(String name, String address, String phone, String email, String status) {
+        super(name, address, phone, email);
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Class: Student, Name: " + super.toString().split(": ")[2];
+    }
+}
+
